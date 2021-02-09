ccecc0a86c519c4301abc676bb5b17399ca0b5aa
victorlaragurmendi <lince00@outlook.com>
2021-02-09 13:38:15 -0500

    Updates

---
+++ precache-manifest.99f366abfbc558ff9e0122d2bb74343e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "232afaeae714787bc6b0cb9c911a4047",
+    "url": "/portafolio/index.html"
+  },
+  {
+    "revision": "7e687b1079f2dc0ce4a6",
+    "url": "/portafolio/static/css/main.ed99b0ae.chunk.css"
+  },
+  {
+    "revision": "3c7189510950ef8f7316",
+    "url": "/portafolio/static/js/2.76472cbc.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portafolio/static/js/2.76472cbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e687b1079f2dc0ce4a6",
+    "url": "/portafolio/static/js/main.6be9149e.chunk.js"
+  },
+  {
+    "revision": "b097f6fd015e184207a4",
+    "url": "/portafolio/static/js/runtime-main.39896225.js"
+  },
+  {
+    "revision": "21b54b389aa10dcf7165b3d8bce9f161",
+    "url": "/portafolio/static/media/BungeeShade-Regular.21b54b38.ttf"
+  },
+  {
+    "revision": "463974da030979d06db194486cc9ce69",
+    "url": "/portafolio/static/media/Flaticon.463974da.ttf"
+  },
+  {
+    "revision": "865a16f2ee6fd5b63b75f2f103712aa3",
+    "url": "/portafolio/static/media/Flaticon.865a16f2.svg"
+  },
+  {
+    "revision": "ad0e7b2b325748cee79babd2aebeeea2",
+    "url": "/portafolio/static/media/Flaticon.ad0e7b2b.woff2"
+  },
+  {
+    "revision": "c3a36b182f24b72796fc4380c1787cd6",
+    "url": "/portafolio/static/media/Flaticon.c3a36b18.eot"
+  },
+  {
+    "revision": "ffb2fa3990adb0a8b0e12c40df072c47",
+    "url": "/portafolio/static/media/Flaticon.ffb2fa39.woff"
+  },
+  {
+    "revision": "16538589cc2966c4fa8d84045ae00897",
+    "url": "/portafolio/static/media/buscadorimagenes.16538589.jpg"
+  },
+  {
+    "revision": "a9eaae7308953e1820c737fd8f7b26f3",
+    "url": "/portafolio/static/media/organizesongs.a9eaae73.png"
+  },
+  {
+    "revision": "0f1a7d31e8d5c81b78a0ee0e44a68fd2",
+    "url": "/portafolio/static/media/rick.0f1a7d31.jpg"
+  },
+  {
+    "revision": "0fdc5bce69582fc2b2d77fbd32bc2373",
+    "url": "/portafolio/static/media/tipodecambio.0fdc5bce.jpg"
+  }
+]);
